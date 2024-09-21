49f182a958d11f0dd54cc2b69e8f79705173a1f6
rinkygarg <rinkygarg@Rinkys-MacBook-Air.local>
2024-09-21 20:36:16 +0530

    new commit

--- fruit.js
+++ fruit.js
@@ -1,6 +1,5 @@
-console.log("Hello World!")
 console.log("123")
-// const lis=document.querySelectorAll('li');
+const lis=document.querySelectorAll('li');
 // lis.appendChild(editb)
 // for(var i=0;i<lis.length;i++){
 //     const editb=document.createElement('button');
@@ -10,7 +9,7 @@ console.log("123")
 //   lis[i].appendChild(editb)
 // }
 // console.log(document.getElementsByClassName)
-const form=document.getElementById('myform');
+const form=document.querySelector('#btn');
 const fruits=document.querySelector('.fruits');
 form.addEventListener('submit',function(event){
     event.preventDefault();
@@ -18,9 +17,9 @@ form.addEventListener('submit',function(event){
    
    // console.log(fruitadd.value)
    const newli=document.createElement('li');
-   console.log(newli)
-   newli.innerHTML=fruitadd.value + `<button class="delete-btn">x</button>`
-   console.log(newli)
+
+   newli.innerHTML=fruitadd.value + <button class="delete-btn">x</button>
+   
    fruits.appendChild(newli);
    console.log("Hello")
 })
